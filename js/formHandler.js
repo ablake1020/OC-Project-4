9c79aa4d441f1bfdae007f95f6df54b5f099a015
Ablake1020 <aaronmb19@gmail.com>
2021-08-24 14:39:55 +0200

    initial updates to code

--- js/formHandler.js
+++ js/formHandler.js
@@ -5,42 +5,39 @@ $(function()
      	preventSubmit: true,
      	submitSuccess: function($form, event)
 	 	{			
-			if(!$form.attr('action')) // Check form doesnt have action attribute
+			if(!$form.attr('action'))
 			{
-				event.preventDefault(); // prevent default submit behaviour
-			
+				event.preventDefault();
 				var processorFile = getProcessorPath($form);
 				var formData = {};
-
-				$form.find("input, textarea, option:selected").each(function(e) // Loop over form objects build data object
+				$form.find("input, textarea, option:selected").each(function(e)
 				{		
 					var fieldData =  $(this).val();
 					var fieldID =  $(this).attr('id');
 				
-					if($(this).is(':checkbox')) // Handle Checkboxes
+					if($(this).is(':checkbox'))
 					{
 						fieldData = $(this).is(":checked");
 					}
-					else if($(this).is(':radio')) // Handle Radios
+					else if($(this).is(':radio'))
 					{
 						fieldData = $(this).val()+' = '+$(this).is(":checked");
 					}
-					else if($(this).is('option:selected')) // Handle Option Selects
+					else if($(this).is('option:selected'))
 					{
 						fieldID = $(this).parent().attr('id');
 					}
 					
 					formData[fieldID] = fieldData;		
 				});
-	
 				$.ajax({
 		        	url: processorFile,
 		    		type: "POST",
 		    		data: formData,
 		    		cache: false,
-		    		success: function() // Success
+		    		success: function()
 		 			{  
-						if($form.is('[success-msg]')) // Show Success Message
+						if($form.is('[success-msg]'))
 						{
 							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('success-msg')+"</strong></div></div>");
 						}
@@ -49,9 +46,9 @@ $(function()
 							window.location.replace($form.attr('success-url'));
 						}	
 						
-						$form.trigger("reset"); // Clear Form	
+						$form.trigger("reset");	
 		 	   		},
-			   		error: function() // Fail
+			   		error: function()
 			   		{
 						if($('#form-alert').length == 0)
 						{
@@ -61,22 +58,19 @@ $(function()
 		   		});
 			}
          },
-         filter: function() // Handle hidden form elements
+         filter: function()
 		 {
 			 return $(this).is(":visible");
          },
 	 });
-	 
-	 // Get Path to processor PHP file
 	 function getProcessorPath(form)
 	 {
 		var path = "./includes/"+form.attr('id')+".php";
 		
-		if(form.attr('template-path')) // Check For Template path
+		if(form.attr('template-path'))
 		{
 			path = form.attr('template-path')+"/includes/"+form.attr('id')+".php";
 		}
-		
 	 	return path
 	 }
 });
